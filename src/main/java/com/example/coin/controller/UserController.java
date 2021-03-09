d56e269b9f81bdc8a2a86b691350e9633b09ec35
夷 巫 <1343296078@qq.com>
2021-03-09 19:31:22 +0800

    feat: 访问网页

---
+++ src/main/java/com/example/coin/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.coin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World !!!";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
