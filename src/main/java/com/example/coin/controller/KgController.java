f08a955f04378a1a2e4982437afb9ce92aef70aa
rx <181250120@smail.nju.edu.cn>
2021-04-17 11:08:07 +0800

    feat:三元组接口

--- src/main/java/com/example/coin/controller/KgController.java
+++ src/main/java/com/example/coin/controller/KgController.java
@@ -2,7 +2,6 @@ package com.example.coin.controller;
 
 import com.example.coin.serviceImpl.KgServiceImpl;
 import com.example.coin.vo.JsonVO;
-import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +11,10 @@ public class KgController {
     @Autowired
     KgServiceImpl kgService;
 
-    @RequestMapping(value = "/getKg",method = RequestMethod.POST)
+    @GetMapping(value = "/{data}/getKg")
     @ResponseBody
-    public JsonVO getKg(@RequestBody String txt){
-        return kgService.getTriple(txt);
+    public JsonVO getKg(@PathVariable String data){
+        return kgService.getTriple(data);
     }
 
 }
